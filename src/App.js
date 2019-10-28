561dba8ce71f12ae250a358db48a00a3d479ebb3
corymorin11 <54879258+corymorin11@users.noreply.github.com>
2019-10-28 15:59:06 -0600

    Final Album App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,135 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Drawer from "@material-ui/core/Drawer";
+import { Link, Route } from "react-router-dom";
+import { auth, db, snapshotToArray } from "./firebase";
+import Photos from "./Photos";
+import AddAlbum from "./AddAlbum";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 export function App(props) {
-  return <div />;
+  const [drawer_open, setDrawerOpen] = useState(false);
+  const [user, setUser] = useState(null);
+  const [dialog_open, setDialogOpen] = useState(false);
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(u => {
+      if (u) {
+        setUser(u);
+      } else {
+        props.history.push("/");
+      }
+      // do something
+    });
+
+    return unsubscribe;
+  }, [props.history]);
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
+        .doc(user.uid)
+        .collection("albums")
+        .onSnapshot(snapshot => {
+          const updated_albums = snapshotToArray(snapshot);
+          setAlbums(updated_albums);
+        });
+    }
+  }, [user]);
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        props.history.push("/");
+      })
+      .catch(error => {
+        alert(error.mesage);
+      });
+  };
+
+  if (!user) {
+    return <div />;
+  }
+
+  return (
+    <div>
+      <AppBar position="static" color="Primary">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            onClick={() => {
+              setDrawerOpen(true);
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            color="inherit"
+            style={{ flexGrow: "1", marginLeft: "30px" }}
+          >
+            My App
+          </Typography>
+          <Typography color="inherit" style={{ marginRight: "30px" }}>
+            Hi! {user.email}
+          </Typography>
+          <Button color="inherit" onClick={handleSignOut}>
+            Sign out
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        open={drawer_open}
+        onClose={() => {
+          setDrawerOpen(false);
+        }}
+      >
+        <List component="nav">
+          {albums.map(a => {
+            return (
+              <ListItem
+                button
+                to={"/app/album/" + a.id + "/"}
+                component={Link}
+                onClick={() => {
+                  setDrawerOpen(false);
+                }}
+              >
+                <ListItemText primary={a.name} />
+              </ListItem>
+            );
+          })}
+          <ListItem
+            button
+            onClick={() => {
+              setDialogOpen(true);
+            }}
+          >
+            <ListItemText primary="Create New Album" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <AddAlbum
+        open={dialog_open}
+        onClose={() => {
+          setDialogOpen(false);
+        }}
+        user={user}
+      />
+      <Route
+        path="/app/album/:album_id/"
+        render={routeProps => {
+          return <Photos user={user} {...routeProps} />;
+        }}
+      />
+    </div>
+  );
 }
